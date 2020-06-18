67f6c72003f5d2ab229a6ba63b30f28f666dd5f6
Ahmed <ahmed_pule@hotmail.com>
2020-06-18 22:13:43 +0200

    project update

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,19 +11,78 @@ class MyApp extends StatelessWidget {
     // TODO: implement build
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal ,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-            child: Column(
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50.0,
-                  backgroundImage: AssetImage('images/ahmed.jpg'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/ahmed.jpg'),
+              ),
+              Text(
+                'Ahmed Puška',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontFamily: 'Dancing Script',
+                    fontSize: 45.0,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic),
+              ),
+              Text(
+                'JUNIOR DEVELOPER',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: 'Bebas Neue',
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 3.0,
+                  wordSpacing: 5.0,
                 ),
-                Text(
-                  'Ahmed Puška'
-                )
-              ],
-            ),
+              ),
+              SizedBox(
+                height: 20.0,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+                width: 150.0,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                    size: 30,
+                  ),
+                  title: Text(
+                    '+ 00 387 62 052 545',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.teal.shade900),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    size: 30,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'ahmed_pule@hotmail.com',
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20.0,
+                        color: Colors.teal.shade900),
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
